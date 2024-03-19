2a5c8e82adf6ca1ed07fca3f7a97cbc4b8c703f2
vivianequeiroz <vivianedsqueiroz@gmail.com>
2024-03-19 16:16:24 -0300

    initial structure

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./main.css";
+
+function App() {
+  return <>test</>;
+}
+
+export default App;
